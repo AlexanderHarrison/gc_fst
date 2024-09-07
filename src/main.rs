03be2c30752fdf1a5f60e681f846350a61256ca6
Alex Harrison <alexander.harrison.r@gmail.com>
2024-09-06 19:45:11 -0600

    rom size check

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,84 @@
+use gc_fst::*;
+
+const HELP: &'static str = 
+"Usage: gc_fst extract <iso path>
+       gc_fst rebuild <root path> [iso path]";
+
+fn usage() -> ! {
+    eprintln!("{}", HELP);
+    std::process::exit(1);
+}
+
 fn main() {
-    match std::env::args().nth(1).as_deref().unwrap() {
-        "unpack" => {
-            let iso_path = std::env::args().nth(2).expect("iso path not passed");
-            let iso = std::fs::read(&iso_path).unwrap();
-            gc_fst::read_iso(&iso).unwrap();
+    let args = std::env::args().collect::<Vec<_>>();
+    match args.get(1).map(|s| s.as_str()) {
+        Some("extract") => {
+            let iso_path = match args.get(2).map(|s| s.as_str()) {
+                Some(p) => p,
+                None => usage(),
+            };
+
+            let iso = match std::fs::read(&iso_path) {
+                Ok(i) => i,
+                Err(e) => {
+                    eprintln!("Error: Could not read iso: {}", e);
+                    std::process::exit(1);
+                }
+            };
+
+            match read_iso(&iso) {
+                Ok(_) => (),
+                Err(ReadISOError::RootDirNotEmpty) => {
+                    eprintln!("Error: root directory is not empty");
+                    std::process::exit(1);
+                }
+                Err(ReadISOError::InvalidISO) => {
+                    eprintln!("Error: iso path does not exist");
+                    std::process::exit(1);
+                },
+                Err(ReadISOError::WriteFileError(e)) => {
+                    eprintln!("Error: Could not write file: {}", e);
+                    std::process::exit(1);
+                },
+                Err(ReadISOError::CreateDirError(e)) => {
+                    eprintln!("Error: Could not create directory: {}", e);
+                    std::process::exit(1);
+                },
+            }
         }
-        "rebuild" => {
-            let bytes = gc_fst::write_iso(std::path::Path::new("root")).unwrap();
-            std::fs::write("out.iso", &bytes).unwrap();
+        Some("rebuild") => {
+            let root_path = match args.get(2).map(|s| s.as_str()) {
+                Some(p) => p,
+                None => usage(),
+            };
+
+            let iso_path = match args.get(3).map(|s| s.as_str()) {
+                Some(p) => p,
+                None => "out.iso",
+            };
+
+            let bytes = match write_iso(std::path::Path::new(root_path)) {
+                Ok(b) => b,
+                Err(WriteISOError::ISOTooLarge) => {
+                    eprintln!("Error: Resulting ISO is too large");
+                    std::process::exit(1);
+                },
+                Err(WriteISOError::InvalidFilename(f)) => {
+                    eprintln!("Error: Filename {:?} cannot be written in an ISO", f);
+                    std::process::exit(1);
+                },
+                Err(WriteISOError::ReadFileError(e)) => {
+                    eprintln!("Error: Could not read file: {}", e);
+                    std::process::exit(1);
+                },
+                Err(WriteISOError::ReadDirError(e)) => {
+                    eprintln!("Error: Could not read directory: {}", e);
+                    std::process::exit(1);
+                },
+            };
+
+            std::fs::write(&iso_path, &bytes).unwrap();
         }
-        s => eprintln!("unk {}", s),
+        _ => usage(),
     }
 }
