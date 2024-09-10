6cec430b072db3860b962217263ea5ec648d5d87
Alex Harrison <alexander.harrison.r@gmail.com>
2024-09-09 22:21:37 -0600

    add set-ID command

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use gc_fst::*;
 
 const HELP: &'static str = 
 "Usage: gc_fst extract <iso path>
-       gc_fst rebuild <root path> [iso path]";
+       gc_fst rebuild <root path> [iso path]
+       gc_fst set-ID <ISO.hdr path | iso path> <game ID>";
 
 fn usage() -> ! {
     eprintln!("{}", HELP);
@@ -12,6 +13,42 @@ fn usage() -> ! {
 fn main() {
     let args = std::env::args().collect::<Vec<_>>();
     match args.get(1).map(|s| s.as_str()) {
+        Some("set-ID") => {
+            let path = match args.get(2).map(|s| s.as_str()) {
+                Some(p) => p,
+                None => usage(),
+            };
+
+            let game_id = match args.get(3).map(|s| s.as_str()) {
+                Some(p) => p,
+                None => usage(),
+            };
+
+            let mut f = match std::fs::File::options().write(true).open(path) {
+                Ok(f) => f,
+                Err(e) => {
+                    eprintln!("Error: Could not open file: {}", e);
+                    std::process::exit(1);
+                }
+            };
+
+            if game_id.len() != 6 
+                || game_id[0..4].chars().any(|c| !c.is_ascii_uppercase()) 
+                || game_id[4..6].chars().any(|c| !c.is_ascii_digit()) 
+            {
+                eprintln!("Error: Invalid game ID: '{}'. Expected ID such as 'GALE01'", game_id);
+                std::process::exit(1);
+            }
+
+            use std::io::Write;
+            match f.write_all(game_id.as_bytes()) {
+                Ok(_) => (),
+                Err(e) => {
+                    eprintln!("Error: Could not write file: {}", e);
+                    std::process::exit(1);
+                }
+            }
+        }
         Some("extract") => {
             let iso_path = match args.get(2).map(|s| s.as_str()) {
                 Some(p) => p,
